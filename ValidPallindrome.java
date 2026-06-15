905222a72d7ab4c82bcce0aee9b900d34c9c3dab
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-15 21:58:17 +0530

    Valid Pallindrome Check

---
+++ ValidPallindrome.java
@@ -0,0 +1,24 @@
+class ValidPallindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length()-1;
+        while(left<right) {
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        ValidPallindrome vp = new ValidPallindrome();
+        System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(vp.isPalindrome("race a car"));  
+    }
+}
