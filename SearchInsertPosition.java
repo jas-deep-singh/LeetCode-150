afdd02f16929203763b74003f639a2b8515fce1d
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-15 22:09:07 +0530

    Search Insert Position or return position for a sorted array

---
+++ SearchInsertPosition.java
@@ -0,0 +1,24 @@
+class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while(low<=high) {
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            else if(nums[mid]<target) {
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        SearchInsertPosition obj = new SearchInsertPosition();
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(obj.searchInsert(nums, target));
+    }
+}
