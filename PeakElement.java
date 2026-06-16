150be7eeb4c66afb85246eb3dfe65b75f41f6c8c
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-16 22:30:09 +0530

    find peak element

---
+++ PeakElement.java
@@ -0,0 +1,34 @@
+class PeakElement {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n==1) {
+            return 0;
+        }
+        if(nums[0]>nums[1]) {
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]) {
+            return n-1;
+        }
+        int low = 1, high = n-2;
+        while(low<=high) {
+            int mid = low+((high-low)/2);
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) {
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]) {
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        PeakElement peakElement = new PeakElement();
+        int[] nums = {1, 2, 3, 1};
+        int peakIndex = peakElement.findPeakElement(nums);
+        System.out.println("Peak element index: " + peakIndex);
+    }
+}
