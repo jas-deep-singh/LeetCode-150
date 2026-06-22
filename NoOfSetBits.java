982ff50871b6e736445f578959f6a1eaaf992b5c
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-22 23:27:18 +0530

    return the number of set bits in the binary representation

---
+++ NoOfSetBits.java
@@ -0,0 +1,16 @@
+class NoOfSetBits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0) {
+            if((n&1)==1) {
+                count+=1;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        NoOfSetBits n = new NoOfSetBits();
+        System.out.println(n.hammingWeight(11)); 
+    }
+}
