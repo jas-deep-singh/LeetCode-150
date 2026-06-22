b732e252ff56810d42034392b5c136b19116d2d0
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-22 22:55:54 +0530

    check if two indices contains the same element such that abs(i - j) <= k

---
+++ ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+class ContainsDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size()>k) {
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+    public static void main(String []args) {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        System.out.println(cd.containsNearbyDuplicate(nums, k));
+    }
+}
