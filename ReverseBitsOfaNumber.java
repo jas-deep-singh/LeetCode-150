0fdf9744ee8756020d1cd5a8553064d82657cae0
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-23 13:43:24 +0530

    reverse bits of a 32 bit number

---
+++ ReverseBitsOfaNumber.java
@@ -0,0 +1,16 @@
+class ReverseBitsOfaNumber {
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i=0;i<32;i++) {
+            result<<=1;
+            result |= (n&1);
+            n>>>=1;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        ReverseBitsOfaNumber obj = new ReverseBitsOfaNumber();
+        int n = 43261596;
+        System.out.println(obj.reverseBits(n));
+    }
+}
