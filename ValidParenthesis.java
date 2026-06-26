abdbfc98725a78ae0bb6eaf2d730235473acb901
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-06-26 18:43:03 +0530

    valid parenthesis

---
+++ ValidParenthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch: s.toCharArray()) {
+            if(ch=='(' || ch=='{' || ch=='[') {
+                stack.push(ch);
+            }
+            else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        ValidParenthesis vp = new ValidParenthesis();
+        String s = "{[()]}";
+        System.out.println(vp.isValid(s)); 
+    }
+}
